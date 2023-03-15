b7b8d69ad9d2a1f97ea4ef18b50bb51efcf50f20
john tom <johntommokaa@gmail.com>
2023-03-15 19:36:56 +0100

    more commit

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,11 +4,32 @@ const userDB= require('../models/userDB')
 
 const loginController = async(req,res)=>{
     const {email, password} = req.body
-    if(!email || !password){
+    if(!email || !password)
         res.send(' please input all fields')
-    }else{
-        const founduser = await userDB.find()
-    }
+    
+    const founduser = await userDB.findOne({email:email})
+    if(!founduser)
+        res.send('error user does not exist')
+    
+    const checkpwd= await bcrypt.compare(password,founduser.password) 
+    if(!checkpwd)
+        res.send('password not correct');        
+    //res.json('logged in')
+    console.log(founduser.password)
+    const accessToken = jwt.sign(founduser.username,process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken:accessToken})
+    const user= founduser.username
 }
 
-module.exports= loginController
+function authenticateToken(req,res,next){
+    const bearerHeader= req.headers['authorization']
+    const bearerToken = bearerHeader && bearerHeader.split(" ")[1]
+    if(bearerToken==null) return res.sendStatus(401)
+    jwt.verify(bearerToken,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+        if(err) res.sendStatus(401)
+        req.user= user
+        next()
+    })
+}
+
+module.exports= {loginController, authenticateToken};
--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,15 @@
 const express= require('express')
 const router= require('express').Router()
 const db = require('../models/userDB')
+const {authenticateToken,loginController}= require('../controllers/loginController')
 
 router.get('/user/login',(req,res)=>{
     res.send('login')
 })
-router.post('/user/login',(req,res)=>{
+router.post('/user/login',loginController,(req,res)=>{
 
 })
-
+router.get('/user/protect',authenticateToken,(req,res,)=>{
+    res.json({message:'access granted'})
+})
 module.exports=router
